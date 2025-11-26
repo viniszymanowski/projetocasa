1fcd6c681ce36d14a5a34c02edae651433983c37
viniszymanowski <66928394+viniszymanowski@users.noreply.github.com>
2025-11-26 11:23:59 -0500

    fix: Mudar para hash-based routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Router } from 'wouter'
+import { useHashLocation } from 'wouter/use-hash-location'
 import App from './App.tsx'
 import './index.css'
 
-// Detectar ambiente: produção (GitHub Pages) ou desenvolvimento (localhost)
-const isProduction = import.meta.env.PROD
-const basePath = isProduction ? '/projetocasa' : ''
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router base={basePath}>
+    <Router hook={useHashLocation}>
       <App />
     </Router>
   </React.StrictMode>,
